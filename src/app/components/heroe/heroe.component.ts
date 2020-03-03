8aa97d61ce53e3ae286527726749d3bb9786eea8
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-03-02 21:20:38 -0400

    20200302 Obtener los valores del heroe apartir del id enviado en la ruta.

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html',
+    styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+    heroe:any = {};
 
-  ngOnInit() {
-  }
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private _heroesService: HeroesService
+    ) {
+        this.activatedRoute.params.subscribe( params => {
+            // console.log( params['id'] );
+            this.heroe = this._heroesService.getHeroe( params['id'] );
+        })
+    }
+
+    ngOnInit() {
+    }
 
 }
