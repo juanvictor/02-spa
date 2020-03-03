3bfc8b3a063707ca712a9db1d6167ef2acd41423
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-03-02 21:06:53 -0400

    20200302 Moverse entre rutas, dos formas.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -11,14 +12,21 @@ export class HeroesComponent implements OnInit {
 
     heroes: Heroe[] = [];
 
-    constructor( private _heroesService:HeroesService ) {
-        console.log("constructor");
+    constructor(
+        private _heroesService:HeroesService,
+        private router:Router
+    ) {
+        // console.log("constructor");
     }
 
     ngOnInit() {
         this.heroes = this._heroesService.getHeroes();
 
-        console.log(this.heroes);
+        // console.log(this.heroes);
     }
 
+    verHeroe( idx:number ){
+        this.router.navigate( ['/heroe', idx] );
+        // console.log(idx);
+    }
 }
